10f9f6369fec8d234c1932a71baa3bc73c634c8c
Polina <polinasidenko44@gmail.com>
2019-10-24 09:51:09 +0300

    first commit

---
+++ o.cpp
@@ -0,0 +1,12 @@
+nclude <iostream>
+#include <cstdlib> // для system
+using namespace std;
+
+int main() 
+{ 
+    cout << "Hello, world!" << endl;
+    system("pause"); // Только для тех, у кого MS Visual Studio
+    return 0; 
+}
+
+
